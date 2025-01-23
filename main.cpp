0693237fb899854ca3aac4bb1ddb80c9b6ac2e8f
Jan Hebbel <jan.hebbel@gmail.com>
2025-01-23 20:48:21 +0100

    Added performance measurements.

--- main.cpp
+++ main.cpp
@@ -14,9 +14,17 @@ int main(void) {
         return 1;
     }
 
-    File file = read_file(&parse_arena, "../res/cube.ob");
+    double start = get_time_in_seconds();
+    File file = read_file(&parse_arena, "../res/cube.obj");
+    double mid = get_time_in_seconds();
     Parse_Result parsed = parse((char*)file.data, file.len);
+    double end = get_time_in_seconds();
     Assert(parsed.success);
+
+    print_format("Read file in %.3f ms\n", (mid - start) * 1000.0);
+    print_format("Parsing took %.3f ms\n", (end - mid) * 1000.0);
+    print_format("_____________________________________________\n");
+    print_format("Overall time: %.3f ms\n", (end - start) * 1000.0);
 
     return 0;
 }
