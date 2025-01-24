283173727d089d040efcfaa75346f5aa961e1843
Jan Hebbel <jan.hebbel@gmail.com>
2025-01-24 17:08:27 +0100

    Tested linking with the rad linker.

--- build.bat
+++ build.bat
@@ -4,9 +4,13 @@ if not exist bin mkdir bin
 pushd bin
 
 :: Debug
-set compile_options=-nologo -std:c++14 -D DEBUG=1 /diagnostics:caret -EHa- -FC -Zi -Od -W4 -WX -wd4201 -wd4090 -wd4100 -wd4127 -external:W0 -I..\inc -Feparse
-set link_options=-incremental:no -subsystem:console
+set compile_options=-c -nologo -std:c++14 -D DEBUG=1 /diagnostics:caret -EHa- -FC -Zi -Od
+set compile_options=%compile_options% -W4 -WX -wd4201 -wd4090 -wd4100 -wd4127 -external:W0
+set compile_options=%compile_options% -I..\inc
+set link_options=-nologo -debug:full -incremental:no -subsystem:console -OUT:parse.exe
 
-cl.exe %compile_options% ..\main.cpp -link %link_options% user32.lib
+cl.exe %compile_options% ..\main.cpp
+link.exe main.obj %link_options% user32.lib
+:: radlink.exe main.obj %link_options% user32.lib
 
 popd
